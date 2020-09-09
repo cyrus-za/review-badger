073ee42270b9afbd6995937f0d7a7069e873ecaf
Nick Lemmon <nick.lemmon@gmail.com>
2020-09-08 21:52:56 -0400

    Add additional error handling

--- src/api.js
+++ src/api.js
@@ -2,13 +2,15 @@ const axios = require('axios')
 const {
   GITHUB_AUTH_HEADER,
   GITHUB_API_URL,
-  GITHUB_ORG,
   GITHUB_REPO,
   SLACK_WEBHOOK_URL,
   SLACK_CHANNEL,
 } = require('./constants.js')
 
 function getPRs() {
+  if (!GITHUB_REPO)
+    throw new Error('No GitHub repository supplied - pull requests cannot be retrieved.')
+
   return axios({
     method: 'GET',
     url: `${GITHUB_API_URL}/repos/${GITHUB_REPO}/pulls`,
@@ -17,6 +19,11 @@ function getPRs() {
 }
 
 function postSlackMsg({ text, blocks } = {}) {
+  if (!SLACK_WEBHOOK_URL)
+    throw new Error('No SLACK_WEBHOOK_URL supplied - messages cannot be posted.')
+
+  if (!SLACK_CHANNEL) throw new Error('No SLACK_CHANNEL supplied - messages cannot be posted.')
+
   return axios({
     method: 'POST',
     url: SLACK_WEBHOOK_URL,
