2a47031e39290e463118bb25ef50c1e88c685c93
Nick Lemmon <nick.lemmon@gmail.com>
2020-12-09 12:59:32 -0500

    Change how token is passed to other request

--- src/api.js
+++ src/api.js
@@ -22,7 +22,7 @@ function getPR(pullNumber) {
     method: 'GET',
     url: `${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/pulls/${pullNumber}`,
     headers: {
-      Authorization: GITHUB_TOKEN,
+      Authorization: `token: ${GITHUB_TOKEN}`,
     },
   }).catch(err => console.log(err))
 }
